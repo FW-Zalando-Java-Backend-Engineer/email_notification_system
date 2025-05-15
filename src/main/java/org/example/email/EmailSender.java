40a430f12c808d4696f0e645abafd0558687fc98
safwan-kher <eng.safwan.kherallah@gmail.com>
2025-05-15 11:14:58 +0200

    EmailSender class is completed

---
+++ src/main/java/org/example/email/EmailSender.java
@@ -0,0 +1,38 @@
+package org.example.email;
+
+
+
+/**
+ * EmailSender simulates the task of sending an email.
+ */
+public class EmailSender implements  Runnable{
+    private final String recipient;
+
+    /**
+     * Constructs an EmailSender for the specified recipient.
+     *
+     * @param recipient the email address of the recipient
+     */
+    public EmailSender(String recipient) {
+        this.recipient = recipient;
+    }
+
+
+
+    /**
+     * Simulates sending an email by printing to the console.
+     */
+    @Override
+    public void run() {
+        System.out.println("Sending email to: "+recipient+" by "+Thread.currentThread().getName());
+        try{
+           Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            System.out.println("Email sending interrupted for: "+recipient);
+            Thread.currentThread().interrupt();
+        }
+
+        System.out.println("Email sent to: " + recipient + " by " + Thread.currentThread().getName());
+
+    }
+}
